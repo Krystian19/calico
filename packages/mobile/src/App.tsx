0ce8aca755640dc57794a3287728cdbb3707b159
Jan Guzman <janfrancisco19@gmail.com>
2022-06-02 08:21:07 -0400

    include tsx file changes

--- packages/mobile/src/App.tsx
+++ packages/mobile/src/App.tsx
@@ -1,12 +1,46 @@
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Views
+import CurrencyListView from './views/CurrencyListVie.tsxw';
+import SettingsView from './views/SettingsView.tsx';
+
+// View Names
+const currencyListView = 'currencyListView';
+const settingsView = 'settingsView';
+
+const Tab = createBottomTabNavigator();
 
 export default function App(): JSX.Element {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={currencyListView}
+        screenOptions={({ route }): BottomTabNavigationOptions => ({
+          tabBarIcon: ({ focused, color, size }): JSX.Element => {
+            let iconName = 'home';
+            const routeName = route.name;
+
+            if (routeName == currencyListView) {
+              iconName = focused ? 'home' : 'home-outline';
+            } else if (routeName == settingsView) {
+              iconName = focused ? 'settings' : 'settings-outline';
+            }
+
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+      >
+        <Tab.Screen name={currencyListView} component={CurrencyListView} />
+        <Tab.Screen name={settingsView} component={SettingsView} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
